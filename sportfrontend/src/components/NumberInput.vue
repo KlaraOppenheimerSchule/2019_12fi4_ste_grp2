ca2fa27ad83c08a16d9d0e2aa1b3724743c79fdf
Tim Weidner <weidner.tim@hotmail.de>
2019-09-20 12:30:45 +0200

    validated input NumberInput.vue

--- sportfrontend/src/components/NumberInput.vue
+++ sportfrontend/src/components/NumberInput.vue
@@ -7,9 +7,10 @@
           <ion-input
             type="number"
             :value="val"
-            @input="val = $event.target.value"
+            @input="inputevent($event.target)"
           ></ion-input>
         </ion-item>
+        <ion-text v-if="error" color="danger">Schülernummer nicht gültig</ion-text>
         <ion-button size="full" v-on:click="routeToId()">Button</ion-button>
       </ion-col>
     </ion-row>
@@ -21,12 +22,27 @@ export default {
   name: "numberinput",
   data: function() {
     return {
-      val: null
+      val: null,
+      error: false
     };
   },
   methods: {
-    routeToId: function() {
-      this.$router.push({ path: "stats/" + this.val });
+      inputevent: function(target){
+          this.val = target.value;
+      },
+      validateInput: function(){
+        let validateVar = false;
+          if(this.val.length == 6){
+              validateVar = true
+          }
+          return validateVar;
+      },
+      routeToId: function() {
+        if(this.validateInput()){
+            this.$router.push({ path: "stats/" + this.val });
+        }else{
+            this.error = true;
+        }
     }
   }
 };
