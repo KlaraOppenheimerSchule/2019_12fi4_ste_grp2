25776e9b08ebbeef7974dd2fb4152a5e33d04201
Flaflo <flaflo@ok.de>
2019-11-14 14:52:41 +0100

    Added sportapp entry point

--- sportbackend/src/main/kotlin/de/kos/sport/Main.kt
+++ sportbackend/src/main/kotlin/de/kos/sport/Main.kt
@@ -1,18 +1,11 @@
 package de.kos.sport
 
-import com.google.gson.GsonBuilder
 import de.kos.sport.database.DBConnector
-import de.kos.sport.database.Student
-import de.kos.sport.database.Students
-import org.jetbrains.exposed.sql.SortOrder
-import org.jetbrains.exposed.sql.transactions.transaction
 
 /**
  * The programs entrypoint
  */
 fun main() {
     DBConnector.init()
-
-
-    //SportsApp.start()
+    SportsApp.start()
 }
