a42a4a30800075cd2ac669fa00735fd625d5fe1c
Flaflo <flaflo@ok.de>
2020-01-13 09:34:59 +0100

    Removed unnecessary try-catch block

--- sportbackend/src/main/kotlin/de/kos/sport/routes/checkpoint/CheckpointCreateRoute.kt
+++ sportbackend/src/main/kotlin/de/kos/sport/routes/checkpoint/CheckpointCreateRoute.kt
@@ -10,39 +10,35 @@ class CheckpointCreateRoute : Route {
     override fun handle(req: Request, response: Response): Any {
         val sb = StringBuilder("[")
 
-        try {
-            val token = req.splat()[0]
+        val token = req.splat()[0]
 
-            val name = req.splat()[1]
-            val location = req.splat()[2]
-            val userId = req.splat()[3].toIntOrNull()
-            val score = req.splat()[4].toIntOrNull()
+        val name = req.splat()[1]
+        val location = req.splat()[2]
+        val userId = req.splat()[3].toIntOrNull()
+        val score = req.splat()[4].toIntOrNull()
 
-            if (DBConnector.validateToken(token)) {
-                val adminUser =
-                    transaction { DBConnector.getSessionFromToken(token)!!.user }
+        if (DBConnector.validateToken(token)) {
+            val adminUser =
+                transaction { DBConnector.getSessionFromToken(token)!!.user }
 
-                if (adminUser.type == DBConnector.ACCESS_LEVEL_GLOBAL) {
-                    if (score == null) {
-                        sb.append("{ \"error\": \"Score needs to be an integer\" }")
-                    } else if (userId == null) {
-                        sb.append("{ \"error\": \"User needs to be an integer\" }")
-                    } else {
-                        val user = DBConnector.getUserById(userId)
-
-                        if (user == null) {
-                            sb.append("{ \"error\": \"User not found\" }")
-                        } else {
-                            val checkpoint = DBConnector.createCheckpoint(name, location, score, user)
-                            sb.append(checkpoint)
-                        }
-                    }
+            if (adminUser.type == DBConnector.ACCESS_LEVEL_GLOBAL) {
+                if (score == null) {
+                    sb.append("{ \"error\": \"Score needs to be an integer\" }")
+                } else if (userId == null) {
+                    sb.append("{ \"error\": \"User needs to be an integer\" }")
                 } else {
-                    sb.append("{ \"error\": \"Insufficient permissions\" }")
+                    val user = DBConnector.getUserById(userId)
+
+                    if (user == null) {
+                        sb.append("{ \"error\": \"User not found\" }")
+                    } else {
+                        val checkpoint = DBConnector.createCheckpoint(name, location, score, user)
+                        sb.append(checkpoint)
+                    }
                 }
+            } else {
+                sb.append("{ \"error\": \"Insufficient permissions\" }")
             }
-        } catch (ex: Exception) {
-            ex.printStackTrace()
         }
 
         return sb.append("]").toString()
