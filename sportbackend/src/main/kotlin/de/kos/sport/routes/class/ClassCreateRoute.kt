d64e96dcec72743f217ac01b3fc43af6cffcd229
Flaflo <flaflo@ok.de>
2020-03-06 09:44:21 +0100

    Throwing error if class already exists

--- sportbackend/src/main/kotlin/de/kos/sport/routes/class/ClassCreateRoute.kt
+++ sportbackend/src/main/kotlin/de/kos/sport/routes/class/ClassCreateRoute.kt
@@ -5,6 +5,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 import spark.Request
 import spark.Response
 import spark.Route
+import java.sql.SQLException
 
 class ClassCreateRoute : Route {
     override fun handle(req: Request, response: Response): Any {
@@ -17,8 +18,14 @@ class ClassCreateRoute : Route {
             val adminUser = transaction { DBConnector.getSessionFromToken(token)!!.user }
 
             if (adminUser.type == DBConnector.ACCESS_LEVEL_GLOBAL) {
-                val generatedClass = DBConnector.createClass(className)
-                sb.append("{ \"class\": ${generatedClass.id} }")
+                try {
+                    val generatedClass = DBConnector.createClass(className)
+                    sb.append("{ \"class\": ${generatedClass.id} }")
+                } catch (ex: SQLException) {
+                    //The best way at the moment to detect if a user already exists when the query fails
+                    //without forcing an additional sql statement
+                    sb.append("{ \"error\": \"Class already exists\" }")
+                }
             } else {
                 sb.append("{ \"error\": \"Insufficient permissions\" }")
             }
